e6768b77bdbf0dc7a7bdfb6eb367ca01a9a3f6ad
carlo <the7thcapo@gmail.com>
2015-12-25 22:07:24 +1100

    Random Number and 3 IF statements
    check that the number user guess is correct or not
    
    use 3 if statements to display correct label

---
+++ variable/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.txtbxGuessNumber = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnPlayAgain = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCheck.Location = New System.Drawing.Point(62, 195)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(244, 62)
+        Me.btnCheck.TabIndex = 0
+        Me.btnCheck.Text = "Check Guess"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput.Location = New System.Drawing.Point(49, 32)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(461, 39)
+        Me.lblOutput.TabIndex = 1
+        Me.lblOutput.Text = "Enter Guess Here: ( 0 - 100 )"
+        '
+        'txtbxGuessNumber
+        '
+        Me.txtbxGuessNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtbxGuessNumber.Location = New System.Drawing.Point(62, 108)
+        Me.txtbxGuessNumber.Name = "txtbxGuessNumber"
+        Me.txtbxGuessNumber.Size = New System.Drawing.Size(212, 49)
+        Me.txtbxGuessNumber.TabIndex = 2
+        Me.txtbxGuessNumber.Text = "0"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(55, 282)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(153, 39)
+        Me.lblResult.TabIndex = 3
+        Me.lblResult.Text = "RESULT"
+        '
+        'btnPlayAgain
+        '
+        Me.btnPlayAgain.Location = New System.Drawing.Point(242, 348)
+        Me.btnPlayAgain.Name = "btnPlayAgain"
+        Me.btnPlayAgain.Size = New System.Drawing.Size(75, 23)
+        Me.btnPlayAgain.TabIndex = 4
+        Me.btnPlayAgain.Text = "Play Again"
+        Me.btnPlayAgain.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(554, 434)
+        Me.Controls.Add(Me.btnPlayAgain)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtbxGuessNumber)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCheck As System.Windows.Forms.Button
+    Friend WithEvents lblOutput As System.Windows.Forms.Label
+    Friend WithEvents txtbxGuessNumber As System.Windows.Forms.TextBox
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents btnPlayAgain As System.Windows.Forms.Button
+
+End Class
---
+++ variable/Form1.vb
@@ -0,0 +1,53 @@
+﻿Option Strict On
+Public Class Form1
+
+    Dim intComputerGuess As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'fill computer guess with a random number
+        'if correct make a new random number
+        ' Initialize the random-number generator.
+        Randomize()
+        intComputerGuess = CInt(Rnd() * 101)
+        MsgBox(intComputerGuess)
+
+    End Sub
+
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+
+        'check that the number user guess is correct or not
+        'use 3 if statements to display corrrect label
+        'if correct make a new random number
+        Dim intUserGuess As Integer
+        If Integer.TryParse(txtbxGuessNumber.Text, intUserGuess) Then
+            MsgBox("you entered: " + intUserGuess.ToString)      'can change + with &
+        Else
+            MsgBox("please enter a valid number!!!")
+        End If
+
+        If intUserGuess = intComputerGuess Then
+            lblResult.Text = "Congratulations! You got it"
+        End If
+
+        If intUserGuess > intComputerGuess Then
+            lblResult.Text = "HIGH"
+        End If
+
+        If intUserGuess < intComputerGuess Then
+            lblResult.Text = "LOW"
+        End If
+
+        If intUserGuess < 0 Or intUserGuess > 100 Then
+            lblResult.Text = "Please enter 0 to 100"
+        End If
+
+
+    End Sub
+
+    Private Sub btnPlayAgain_Click(sender As Object, e As EventArgs) Handles btnPlayAgain.Click
+
+        Controls.Clear()
+        InitializeComponent()
+
+    End Sub
+End Class
